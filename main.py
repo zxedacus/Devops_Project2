2f6087e291249defe82cbc8618d1117011f6a66d
Zxedacus <64056958+zxedacus@users.noreply.github.com>
2022-09-18 16:20:46 +0800

    html parser

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#This is the main python file.
+import requests
+from bs4 import BeautifulSoup
+
+with open("index.html", "r") as f:
+    doc = BeautifulSoup(f, "html.parser")
+
+url = 'https://finance.yahoo.com/cryptocurrencies/'
+r = requests.get(url)
+
+print(doc)
