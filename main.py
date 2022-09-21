7ee6bce0456b77eff7142740682cb666a2ed7041
zxedacus <fei@xcovad.com>
2022-09-21 19:54:27 +0800

    Added_VisualizationGraphs

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
-
+import matplotlib.pyplot as plt
 
 url = 'https://www.investing.com/indices/us-spx-500-historical-data'
 result = requests.get(url)
 doc = BeautifulSoup(result.text, 'lxml')
 
-
-
+x = []
+y = []
 
 tradingDate = doc.find_all('time') 
 #tradingDate1 = tradingDate[0].getText()
 #print(tradingDate1)
 
 for d in tradingDate:
-
+    x.append(d.getText())
     #print(d.getText())
 
 price = doc.find_all('td', {'class':'datatable_cell__3gwri datatable_cell--align-end__Wua8C datatable_cell--down__2CL8n font-bold'}) 
@@ -22,5 +22,14 @@ price = doc.find_all('td', {'class':'datatable_cell__3gwri datatable_cell--align
 #print(price1)
 
 for p in price:
-    print(p.getText())
+    y.append(p.getText())
+    #print(p.getText())
+    
+
+plt.plot(x,y)
+
+plt.xlabel('Dates')
+plt.ylabel('Returns')
+plt.title('S&P 500')
+plt.show()
 
