e7a9e62ef5f03e1e7637fe747e980b602b5da216
zxedacus <fei@xcovad.com>
2022-09-21 20:45:57 +0800

    still_troubleshooting

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ doc = BeautifulSoup(result.text, 'lxml')
 
 x = []
 y = []
+#y = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
 
 tradingDate = doc.find_all('time') 
 #tradingDate1 = tradingDate[0].getText()
@@ -17,19 +18,21 @@ for d in tradingDate:
     x.append(d.getText())
     #print(d.getText())
 
-price = doc.find_all('td', {'class':'datatable_cell__3gwri datatable_cell--align-end__Wua8C datatable_cell--down__2CL8n font-bold'}) 
+price = doc.find_all('td', {'class':'datatable_cell__3gwri datatable_cell--align-end__Wua8C'}) 
 #price1 = price[0].getText()
 #print(price1)
 
 for p in price:
-    y.append(p.getText())
-    #print(p.getText())
+#     y.append(p.getText())
+    print(p.getText())
     
+# print(x)
+# print(y)
 
-plt.plot(x,y)
+# plt.plot(x,y)
 
-plt.xlabel('Dates')
-plt.ylabel('Returns')
-plt.title('S&P 500')
-plt.show()
+# plt.xlabel('Dates')
+# plt.ylabel('Returns')
+# plt.title('S&P 500')
+# plt.show()
 
