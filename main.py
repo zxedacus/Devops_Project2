8aff841833b9145afe6887761e3238b2dfa17ba1
zxedacus <fei@xcovad.com>
2022-09-21 16:17:12 +0800

    Scrapped_worldIndices_data

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://finance.yahoo.com'
+url = 'https://finance.yahoo.com/world-indices'
 result = requests.get(url)
 doc = BeautifulSoup(result.text, 'lxml')
 
-title = doc.find_all('h3', {'class':'Fz(14px)--md1100'})
-#title1 = title[0].getText()
+title = doc.find_all('tr', {'class':'simpTblRow'})
+title1 = title[0].getText()
+#print(title1)
 
 for t in title:
     print(t.getText())
 
-#strong = parent.find("strong")
+
 
 
